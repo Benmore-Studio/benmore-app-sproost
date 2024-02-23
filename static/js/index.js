0adb999507ed253f8674e86667fd58f31296ab17
Precioussoul <habsoff@gmail.com>
2024-02-23 18:58:30 +0100

    feat: working on signup mobile-focused

--- static/js/index.js
+++ static/js/index.js
@@ -6,6 +6,7 @@ window.addEventListener("DOMContentLoaded", () => {
 function showStep(n) {
   // This function will display the specified tab of the form...
   var x = document.getElementsByClassName("step")
+
   x[n].style.display = "block"
   //... and fix the Previous/Next buttons:
   if (n === 0) {
@@ -14,39 +15,47 @@ function showStep(n) {
     document.getElementById("prevBtn").style.display = "block"
   }
   if (n == x.length - 1) {
-    document.getElementById("nextBtn").innerHTML = "Submit"
+    document.getElementById("nextBtn").innerHTML = "Complete"
   } else if (n === 0) {
     document.getElementById("nextBtn").textContent = "Continue"
   } else {
     document.getElementById("nextBtn").textContent = "Next"
   }
-  //... and run a function that will display the correct step indicator:
-  fixStepIndicator(n)
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x,
+    y,
+    i,
+    valid = false
+  x = document.getElementsByClassName("step")
+  y = x[currentStep].getElementsByTagName("input")
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    if (y[i].type === "radio" && y[i].checked) {
+      valid = true
+    } else if (y[i].type !== "radio" && y[i].value !== "") {
+      valid = true
+    }
+  }
+
+  return valid // return the valid status
 }
 
 function nextPrev(n) {
   // This function will figure out which tab to display
   var x = document.getElementsByClassName("step")
+  if (n == 1 && !validateForm()) return false
   // Hide the current tab:
   x[currentStep].style.display = "none"
   // Increase or decrease the current tab by 1:
   currentStep = currentStep + n
   // if you have reached the end of the form...
-  if (currentStep >= x.length) {
+  if (currentStep >= x.length - 1) {
     // ... the form gets submitted:
-    document.getElementById("registerForm").submit()
+    document.getElementById("signupForm").submit()
     return false
   }
   showStep(currentStep)
-}
-
-function fixStepIndicator(n) {
-  // This function removes the "active" class of all steps...
-  var i,
-    x = document.getElementsByClassName("dot-step")
-  for (i = 0; i < x.length; i++) {
-    x[i].className = x[i].className.replace(" active", "")
-  }
-  //... and adds the "active" class on the current step:
-  x[n].className += " active"
 }
