be2dde54dd0dd2c5c9728f86cfbdf1bc4a52f5a6
Precioussoul <habsoff@gmail.com>
2024-03-12 18:20:22 +0100

    feat: implementing upload-files logic

--- static/js/requestQuotes.js
+++ static/js/requestQuotes.js
@@ -67,6 +67,7 @@ function clearSuccessMsg() {
 }
 
 function uploadFiles() {
+    const fileContainer = document.getElementById('file-container')
     document.getElementById('upload-quote').addEventListener('change', function() {
         let fileInput = document.getElementById('upload-quote');
         let selectedFiles = fileInput.files
@@ -75,11 +76,9 @@ function uploadFiles() {
         let photos = [];
         let videos = [];
         for (var i = 0; i < selectedFiles.length; i++) {
-            selectedFileNames += selectedFiles[i].name + '<br>';
             selectedFileList.push(selectedFiles[i])
         }
         console.log({ selectedFiles });
-        console.log({ selectedFileNames });
         console.log({ selectedFileList });
 
         if (selectedFileList.length > 0) {
@@ -87,6 +86,28 @@ function uploadFiles() {
             console.log({ photos });
             videos = selectedFileList.filter((file) => file.type.startsWith('video/'))
             console.log({ videos });
+
+            photos.forEach(function(photo) {
+                fileContainer.innerHTML += `
+                <a href='${URL.createObjectURL(photo)}' class="w-[74px] h-[74px] rounded-md" download>
+                    <img src="${URL.createObjectURL(photo)}" alt="" class="w-full h-full object-cover rounded-md">
+                </a>
+                `
+            });
+
+            videos.forEach(function(video) {
+
+                fileContainer.innerHTML +=
+                    `
+               <div class="w-[74px] h-[74px] rounded-md relative">
+                <img src="/static/images/vid-play-bg.jpg" alt="" class="w-full h-full object-cover rounded-md">
+              <a href='${URL.createObjectURL(video)}' class="absolute top-5 left-5 right-5" download >                
+              <img src="/static/svgs/Play-button.svg" alt="">
+              </a>
+           </div>
+               `
+
+            });
         }
     });
 }
