bfa9046ef250e6d8edb40e7b1cfe3d26d3c4cd8e
semper44 <emmanuelcopeters@gmail.com>
2024-04-11 16:12:40 -0700

    emmanuelcopeters/ben3-36 rerefactored this view so that only one view handles all the edit profile for differennt users, added an edit page for home users, so they can edit thier profiles and modified the home page topbar

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,7 +1,18 @@
 from django import forms
-from .models import ContractorProfile
+
+from phonenumber_field.formfields import PhoneNumberField
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
+
+from .models import ContractorProfile, UserProfile
 
 class ContractorProfileForm(forms.ModelForm):
     class Meta:
         model = ContractorProfile
         fields = ['company_name', 'registration_number', 'specialization', 'company_address', 'city']
+
+class HomeOwnersEditForm(forms.ModelForm):
+    phone_number = PhoneNumberField(required=False, widget=PhoneNumberPrefixWidget(initial='US'))
+    class Meta:
+        model = UserProfile
+        fields = [ 'address', 'city', 'state_province', 'phone_number']
+        # fields = "__all__"
