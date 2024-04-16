1f3556f0958afd099815e96eff8760a90960c72b
Free-Ralph <okosachideragia@gmail.com>
2024-04-16 15:21:20 +0100

    fixed a bug

--- property/views.py
+++ property/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render, redirect
 from quotes.models import Project, QuoteRequest
 from django.contrib.auth import get_user_model
 from django.contrib import messages
+from .models import AssignedAccount
 
 User = get_user_model()
 
 def agents_home_owner_account(request, pk):
     try:
         home_owner = User.objects.get(pk=pk)
+        if not AssignedAccount.objects.filter(assigned_by = home_owner, assigned_to = request.user).exists():
+            messages.error(request, "you were not assigned by this user")
+            return  redirect('main:home')
+        
         quotes = QuoteRequest.objects.filter(user=home_owner)
         projects = Project.objects.filter(quote_request__user=home_owner)
         context = {
