9b6d7ff8874ccc7c8bddf061e7065f4737907255
Free-Ralph <okosachideragia@gmail.com>
2024-04-17 17:06:05 +0100

    updated code

--- profiles/models.py
+++ profiles/models.py
@@ -5,12 +5,12 @@ from django.contrib.contenttypes.fields import GenericRelation
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = 'user_profile')
-    address = models.CharField(max_length = 50, null = True, blank = True)
+    address = AddressField()
     city = models.CharField(max_length = 50, null = True, blank = True)
     state_province = models.CharField(max_length = 50, null = True, blank = True)    
     
     def __str__(self):
-        return self.user.username
+        return self.user.email
 
 
 class ContractorProfile(models.Model):
