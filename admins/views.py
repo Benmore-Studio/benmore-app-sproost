87e1ae3e94db185ca93657d0cb0e79dcaddbe723
Nodebe Ikem <nodywelete3@gmail.com>
2024-04-24 14:55:03 +0100

    admin approve project request

--- admins/views.py
+++ admins/views.py
@@ -6,7 +6,7 @@ from django.core.paginator import Paginator
 # Create your views here.
 def projectRequest(request):
     project_history = QuoteRequest.objects.all()
-    paginator = Paginator(project_history, 10)
+    paginator = Paginator(project_history, 1)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
