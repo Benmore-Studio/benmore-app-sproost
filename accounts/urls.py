431bb73f0e544216b850b1bcae5ce9faa5c8d198
semper44 <emmanuelcopeters@gmail.com>
2024-06-10 11:16:07 +0100

    Reference latestbugfix/emma: added the necessary urls for referal signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
-from .views import CustomLoginView, validate_phone_numbers, logout_user
+
+from .views import CustomLoginView, validate_phone_numbers, logout_user, CustomSignupView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='account_login'),
+    path('signup/', CustomSignupView.as_view(), name='account_signup'),
     path('validate-phone', validate_phone_numbers , name='validate_number'),
     path('logout', logout_user, name='logout'),
+    # path('referral/', referral_dashboard, name='referral_dashboard'),
+
     # other urls...
 ]
