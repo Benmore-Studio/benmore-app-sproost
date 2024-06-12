203d38e8aae896c69bcfeefd3e016107fa460877
semper44 <emmanuelcopeters@gmail.com>
2024-06-12 01:31:24 +0100

    Reference latestbugfix/emma: added the routing of home owners with slug

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'main'
 urlpatterns = [
     path('', views.home, name="home"),
-    path('<str:name>/', views.home_owner_with_slug_name, name="home_owner_with_slug_name"),
+    path('home-owners/<str:name>/', views.home_owner_with_slug_name, name="home_owner_with_slug_name"),
     path('menu/', views.MenuList, name="menu"),
     path('quotes-summary/', views.QuotationReturn, name="quotes-summary"),
     path('assign-agent/', views.AssignAgentView.as_view(), name="assign-agent"),
