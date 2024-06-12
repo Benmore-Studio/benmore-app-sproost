c7998de896c44810d111b9680321f538a6e6fd8b
semper44 <emmanuelcopeters@gmail.com>
2024-06-13 00:17:18 +0100

    Fixed latestbugfix/emma: removed these-/settings/production.py from my git ignore

---
+++ settings/production.py
@@ -0,0 +1,27 @@
+from .base import *
+import dj_database_url
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    'default': {
+    }
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+
+# Update DATABASES['default'] with the contents of db_from_env
+DATABASES['default'].update(db_from_env)
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST_USER = "benmore@gmail.com"
