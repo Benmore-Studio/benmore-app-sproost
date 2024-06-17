4e684d6293f04c1fa3d645504ff31f2cdac08867
semper44 <emmanuelcopeters@gmail.com>
2024-06-17 22:12:56 +0100

    solved upload button bugs

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,9 @@ from django.utils.crypto import get_random_string
 from phonenumber_field.modelfields import PhoneNumberField
 from quotes.models import Project
 
+from django.utils.text import slugify
+from django.utils.crypto import get_random_string
+
 class UserTypes(models.TextChoices):
     home_owner = "home-owner"
     contractor = "contractor"
@@ -31,10 +34,24 @@ class User(AbstractUser):
         max_length=100, 
         allow_unicode=True) 
     
+    # def save(self, *args, **kwargs):
+    #     if not self.slug:
+    #         slug_base = f"{self.first_name} {self.last_name}" if self.first_name and self.last_name else self.username
+    #         self.slug = slugify(slug_base, allow_unicode=True)
+    #         while User.objects.filter(slug=self.slug).exists():
+    #             self.slug = f"{slugify(slug_base, allow_unicode=True)}-{get_random_string(4)}"
+    #     super().save(*args, **kwargs)
+
+
+
     def save(self, *args, **kwargs):
         if not self.slug:
             slug_base = f"{self.first_name} {self.last_name}" if self.first_name and self.last_name else self.username
-            self.slug = slugify(slug_base, allow_unicode=True)
-            while User.objects.filter(slug=self.slug).exists():
-                self.slug = f"{slugify(slug_base, allow_unicode=True)}-{get_random_string(4)}"
+            base_slug = slugify(slug_base, allow_unicode=True)
+            unique_slug = base_slug
+            count = 1
+            while User.objects.filter(slug=unique_slug).exists():
+                unique_slug = f"{base_slug}-{get_random_string(4)}"
+                count += 1
+            self.slug = unique_slug
         super().save(*args, **kwargs)
