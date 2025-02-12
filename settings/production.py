67859e9a98a23ff1323dae441f45b056d2830093
semper <emmanuelcopeters@gmail.com>
2025-02-12 08:00:14 -0800

    resloving db error

--- settings/production.py
+++ settings/production.py
@@ -19,14 +19,14 @@ DATABASES = {
     'default': {
     }
 }
-DATABASE_URL= config('DATABASE_URL')
-DATABASES = {'default':dj_database_url.parse(DATABASE_URL, conn_max_age=600)}
+# DATABASE_URL= config('DATABASE_URL')
+# DATABASES = {'default':dj_database_url.parse(DATABASE_URL, conn_max_age=600)}
 
-# db_from_env = dj_database_url.config(conn_max_age=600)
 # print(DATABASE_URL)
 
+db_from_env = dj_database_url.config(conn_max_age=600)
 # Update DATABASES['default'] with the contents of db_from_env
-# DATABASES['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
