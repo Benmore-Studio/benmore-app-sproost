8dc4991d7f6e04887da476d28829ca96398a0c7d
Precioussoul <habsoff@gmail.com>
2025-03-14 16:59:43 +0100

    feat: implemented clients list for agents and property of the clients with agent

--- property/urls.py
+++ property/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import PropertyCreateView,PropertyRetrieveView, PropertyUpdateView, UserPropertyListView, PropertyDeleteView, PropertyListAPIView
+from .views import (PropertyCreateView,PropertyRetrieveView, PropertyUpdateView, UserPropertyListView, 
+PropertyDeleteView, PropertyListAPIView, ClientListView, PropertyListForClientView)
 
 
 app_name = 'property'
@@ -11,6 +12,8 @@ urlpatterns = [
     path('<int:pk>/delete', PropertyDeleteView.as_view(), name="property-delete"),
     path('me/all', UserPropertyListView.as_view(), name="contractor-all-properties"),
     path('all', PropertyListAPIView.as_view(), name="contractor-all-properties"),
+    path('agents/me/clients', ClientListView.as_view(), name='agents-clients'),
+    path('clients/<int:client_id>/properties', PropertyListForClientView.as_view(), name='agents-clients')
  ]
 
  
